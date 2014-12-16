3e47bbfcdaf572cee21f8bd435df2610272d8e02
osamie <osamie2002@gmail.com>
2014-12-15 23:46:49 -0500

    a little animation with name and title text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
+    var writeletterbyletter,nameText,showSubtext; 
 
-
+    
 /* Scroll hire me button to contact page */
 	$('.hire-me').click(function(){
 		    $('html, body').animate({
@@ -20,32 +21,34 @@ $(document).ready(function(){
 /* portfolio mixitup */
 
 	$(window).load(function(){
-    var $container = $('.grid-wrapper');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
-    $('.grid-controls li a').click(function(){
-        $('.grid-controls .current').removeClass('current');
-        $(this).addClass('current');
- 
-        var selector = $(this).attr('data-filter');
+        var $container = $('.grid-wrapper');
         $container.isotope({
-            filter: selector,
+            filter: '*',
             animationOptions: {
                 duration: 750,
                 easing: 'linear',
                 queue: false
             }
-         });
-         return false;
+        });
+     
+        $('.grid-controls li a').click(function(){
+            $('.grid-controls .current').removeClass('current');
+            $(this).addClass('current');
+     
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+             });
+             return false;
+        });
+
+
     });
-});
 
 
 /* Magnific Popup */
@@ -97,42 +100,60 @@ $('.chart').waypoint(function() {
 				{ src:'img/slider/02.jpg', fade:1000 }
 			  ]
 			})('overlay', {
-			  src:'img/overlays/16.png'
+			  src:'img/overlays/16.png',
+              opacity: 1
 			});
-			/*$( "#vegas-next" ).click(function() {
-			  $.vegas('next');
-			});
-			$( "#vegas-prev" ).click(function() {
-			  $.vegas('previous');
-                
-		});*/
+
+			$( "#vegas-next" ).hide(); 
+			$( "#vegas-prev" ).hide(); 
 
 
 /*Contact form */
       $('#contact-form').validate({
-        rules: {
-            name: {
-                minlength: 2,
-                required: true
+            rules: {
+                name: {
+                    minlength: 2,
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    minlength: 2,
+                    required: true
+                }
             },
-            email: {
-                required: true,
-                email: true
+            highlight: function (element) {
+                $(element).closest('.control-group').removeClass('success').addClass('error');
             },
-            message: {
-                minlength: 2,
-                required: true
+            success: function (element) {
+                element.text('OK!').addClass('valid')
+                    .closest('.control-group').removeClass('error').addClass('success');
             }
-        },
-        highlight: function (element) {
-            $(element).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function (element) {
-            element.text('OK!').addClass('valid')
-                .closest('.control-group').removeClass('error').addClass('success');
+        });
+
+
+    nameText = "Zuwa Omigie";
+    showSubtext = function(){
+        $("p#my-title").html("SOFTWARE ENGINEER & WEB DEVELOPER");
+        $("p#my-title").show("slow");
+    };
+
+    writeletterbyletter = function (target, text, index, interval, callback) {      
+        if(index < text.length){
+            $(target).append(text[index++]);
+            setTimeout(function(){writeletterbyletter(target, text, index, interval, callback);},interval);
         }
-    }); 
+
+        if((index === text.length) && (typeof callback === 'function')){
+            callback();
+        }
+    };
 
 
+    $("p#my-title").hide();
+    writeletterbyletter("h1#my-name",nameText,0,200, showSubtext);
 
 });
+
